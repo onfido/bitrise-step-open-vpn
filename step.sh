b9586101fd33f3cbe862eece6ac176aae6fe7240
Mikko Caldara <mikko.caldara@onfido.com>
2019-06-13 16:07:21 +0100

    Run in background

--- step.sh
+++ step.sh
@@ -47,7 +47,7 @@ EOF
     echo ${ca_crt} | base64 -D -o ca.crt > /dev/null 2>&1
     echo ${user_pass} | base64 -D -o login.conf > /dev/null 2>&1
 
-    sudo openvpn --client --route-nopull --route ${subnet1} 255.255.224.0 --route ${subnet2} 255.255.224.0 --dhcp-option DNS ${dns1} --dhcp-option DNS ${dns2} --dev tun --proto ${proto} --remote ${host} ${port} --remote-random-hostname --resolv-retry infinite --nobind --persist-key --persist-tun --remote-cert-tls server --ca ca.crt --verb 3 --auth-user-pass login.conf --reneg-sec 0
+    sudo openvpn --client --route-nopull --route ${subnet1} 255.255.224.0 --route ${subnet2} 255.255.224.0 --dhcp-option DNS ${dns1} --dhcp-option DNS ${dns2} --dev tun --proto ${proto} --remote ${host} ${port} --remote-random-hostname --resolv-retry infinite --nobind --persist-key --persist-tun --remote-cert-tls server --ca ca.crt --verb 3 --auth-user-pass login.conf --reneg-sec 0 > /dev/null 2>&1 &
 
     sleep 5
 
