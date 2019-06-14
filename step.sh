29011e63a3dc4fae3b15e1733b11ec85b91f606f
Mikko Caldara <mikkoc@users.noreply.github.com>
2019-06-14 12:08:46 +0100

    Adjust OpenVPN parameters (#1)
    * Adjust VPN config for our 3rd party VPN access

--- step.sh
+++ step.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
 set -eu
 
+    cat <<EOF > client.ovpn
+client
+dev tun
+route-nopull
+route ${subnet1} 255.255.224.0
+route ${subnet2} 255.255.224.0
+proto ${proto}
+remote ${host} ${port}
+remote-random-hostname
+resolv-retry infinite
+nobind
+persist-key
+persist-tun
+remote-cert-tls server
+verb 3
+ca ca.crt
+auth-user-pass login.conf
+reneg-sec 0
+EOF
+
 case "$OSTYPE" in
   linux*)
     echo "Configuring for Ubuntu"
 
     echo ${ca_crt} | base64 -d > /etc/openvpn/ca.crt
-    echo ${client_crt} | base64 -d > /etc/openvpn/client.crt
-    echo ${client_key} | base64 -d > /etc/openvpn/client.key
+    echo ${user_pass} | base64 -d > /etc/openvpn/login.conf
 
-    cat <<EOF > /etc/openvpn/client.conf
-client
-dev tun
-proto ${proto}
-remote ${host} ${port}
-resolv-retry infinite
-nobind
-persist-key
-persist-tun
-comp-lzo
-verb 3
-ca ca.crt
-cert client.crt
-key client.key
-EOF
+    sudo cp client.ovpn /etc/openvpn/client.conf
 
     service openvpn start client > /dev/null 2>&1
     sleep 5
@@ -40,13 +45,9 @@ EOF
     echo "Configuring for Mac OS"
 
     echo ${ca_crt} | base64 -D -o ca.crt > /dev/null 2>&1
-    echo ${client_crt} | base64 -D -o client.crt > /dev/null 2>&1
-    echo ${client_key} | base64 -D -o client.key > /dev/null 2>&1
-    echo ${user_pass} | base64 -D -o user-pass > /dev/null 2>&1
-    echo ${tls_auth} | base64 -D -o tls-auth.key > /dev/null 2>&1
+    echo ${user_pass} | base64 -D -o login.conf > /dev/null 2>&1
 
-    sudo openvpn --client --dev tun --proto ${proto} --remote ${host} ${port} --resolv-retry infinite --nobind --persist-key --persist-tun --ca ca.crt --cert client.crt --key client.key --ns-cert-type server --comp-lzo --verb 3 --auth-user-pass user-pass --tls-auth tls-auth.key 1 > /dev/null 2>&1 &
-
+    sudo openvpn --config client.ovpn > /dev/null 2>&1 &
     sleep 5
 
     if ifconfig -l | grep utun0 > /dev/null
